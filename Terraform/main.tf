6b375a1fce4eb7a382ad0f462761bfcd8f124c7a
Gyinae <dev.gyinae@gmail.com>
2026-04-17 12:18:13 +0000

    main

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,9 +1,32 @@
 #AWS CONFIGURE LIST - use this to check for your set credentials
 
 provider "aws" {
-	region = ""
+	region = "eg. eu-west-3 "
 	access_key = ""
 	secret_key = "" 
+}
+
+resource "aws_vpc" "name_of_VPC" {
+	cidr_block = "eg. 10.0.0.0/16"
+}
+
+resource "aws_subnet" "name_of_subnet" {
+	vpc_id = aws_vpc.name_of_VPC.id
+	cidr_block = "eg. 10.0.10.0/24"
+	availability_zone = "eg. eu-west-3a"
+}
+
+
+# Creating resources into a default vpc
+
+data "aws_vpc" "eg. existing_vpc" {
+	default = true
+}
+
+resource "aws_subnet" "name_of_subnet_2" {
+        vpc_id = data.aws_vpc.existing_vpc.id
+        cidr_block = "eg. refer default id"
+        availability_zone = "eg. eu-west-3a"
 }
 
 
